e6349bd86911bc8ec24a4cae5eb96899afa61418
Yurei TZK <me@yureitzk.com>
2025-12-17 20:37:24 +0300

    fix: wrapping text button disappears on mobile browsers like Chrome

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -17,13 +17,30 @@ export function detectOveflow(element: Element): boolean {
 	}
 }
 
-export const hasFocus = (ele: HTMLElement | null): boolean => (ele === document.activeElement);
+export function windowDebounce(fn: Function, delay: number) {
+	let timeoutId: number | undefined;
+
+	return (...args: any[]) => {
+		if (timeoutId) {
+			window.clearTimeout(timeoutId);
+		}
+		timeoutId = window.setTimeout(() => fn(...args), delay);
+	};
+}
+
+export const hasFocus = (ele: HTMLElement | null): boolean =>
+	ele === document.activeElement;
 
 export const getDocHeight = () => Math.floor(document.body.clientHeight);
 export const getTocLinkFromHeading = (h: HTMLElement): HTMLElement | null =>
 	document.querySelector<HTMLAnchorElement>(`#toc-list a[href="#${h.id}"]`);
-export const getTocParentLinkFromTocLink = (l: HTMLElement | null): HTMLElement | null =>
-	l?.closest<HTMLLIElement>('#toc-list > li')?.querySelector<HTMLAnchorElement>(':scope > a') ?? null;
-export const markTocItemActive = (a: HTMLElement) => a?.classList.add('active-toc-item');
-export const markTocItemInactive = (a: HTMLElement) => a?.classList.remove('active-toc-item');
-
+export const getTocParentLinkFromTocLink = (
+	l: HTMLElement | null,
+): HTMLElement | null =>
+	l
+		?.closest<HTMLLIElement>('#toc-list > li')
+		?.querySelector<HTMLAnchorElement>(':scope > a') ?? null;
+export const markTocItemActive = (a: HTMLElement) =>
+	a?.classList.add('active-toc-item');
+export const markTocItemInactive = (a: HTMLElement) =>
+	a?.classList.remove('active-toc-item');
